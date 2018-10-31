d67f5e2a9add34295d428f56c7ff17ed38f069e2
Miriam Villa <21miriamv@gatewayhigh.net>
2018-10-30 17:58:06 -0700

    joshy

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-document.write('Hello world!')
+// Don't worry about this line too much right now. It
+// just uses the svg.js library to prepare to draw shapes.
+// Do notice that it references the id of the div we made
+// in the previous step.
+var drawing = SVG('drawing-area');
+
+// Draw a rectangle. The first number is the width,
+// the second number is the height.
+drawing.rect(100, 200)
+
+
 
